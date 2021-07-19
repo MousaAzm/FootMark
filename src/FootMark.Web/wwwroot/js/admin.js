c5212d414e7b09ce29b2418ee99472e39248410e
Mousa Azimi <mousa.azimi77@gmail.com>
2021-07-19 19:37:55 +0200

    Admin main view /dashboard

---
+++ src/FootMark.Web/wwwroot/js/admin.js
@@ -0,0 +1,13 @@
+﻿
+//dashboard sidebar menu
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
+
+$(document).ready(function () {
+    $("#arrowIcon").click(function () {
+        $('i').toggleClass("fa-angle-right fa-angle-down");
+    });
+});
